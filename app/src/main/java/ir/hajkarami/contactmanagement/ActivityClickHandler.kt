a9dc39a43f0bcf461b1eafbf5eded6b37393909f
mshajkarami <mshajkarami.dev@gmail.com>
2025-03-09 11:42:11 +0330

    fix problem

--- app/src/main/java/ir/hajkarami/contactmanagement/ActivityClickHandler.kt
+++ app/src/main/java/ir/hajkarami/contactmanagement/ActivityClickHandler.kt
@@ -1,4 +1,13 @@
 package ir.hajkarami.contactmanagement
 
-class ActivityClickHandler {
+import android.content.Context
+import android.content.Intent
+import android.view.View
+
+class ActivityClickHandler (private val context: Context) {
+
+    fun onFABClicked(view: View){
+        val intent = Intent(view.context, NewNoteActivity::class.java)
+        context.startActivity(intent)
+    }
 }
